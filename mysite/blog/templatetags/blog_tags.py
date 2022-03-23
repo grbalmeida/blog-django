4f5ec3e97a5158eb9843ca3d727c4952dd3b4dde
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-03-23 16:45:12 -0300

    Filtros de template personalizados

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
 from django import template
+from django.utils.safestring import mark_safe
 from django.db.models import Count
+import markdown
 from ..models import Post
 
 register = template.Library()
@@ -23,3 +25,7 @@ def get_most_commented_posts(count=5):
     return Post.published.annotate(
         total_comments=Count('comments')
     ).order_by('-total_comments')[:count]
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
